a492123cd94926bd8b82f8b3a33d6fd142d9e578
Trần Thuỷ Tiên <thi3nthannho.thuyti3n@gmail.com>
2020-12-29 12:30:25 +0700

    lam thong ke

--- DOAN/Controllers/HomeController.cs
+++ DOAN/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Web.Mvc;
 using System.Web.Security;
 using DOAN.Common;
 using DOAN.Models;
+using DOAN.ModelView;
 
 namespace DOAN.Controllers
 {
@@ -17,9 +18,63 @@ namespace DOAN.Controllers
         {
             NGUOIDUNG user = Session["TaiKhoan"] as NGUOIDUNG;
             if (user != null)
+            {
+                ViewBag.Label = "";
+                ViewBag.Value = "";
+                List<CauHinh> list = new List<CauHinh>();
+                foreach (var item in db.CAUHINHs.Where(x => x.Active == true))
+                {
+                    CauHinh ch = new CauHinh();
+                    ch.IdCauHinh = item.IdCauHinh;
+                    ch.TenCauHinh = item.LOAIDETAI.TenLoai + " " + item.NIENKHOA1.NamBD;
+                    list.Add(ch);
+                }
+                ViewBag.GiaTri = 0;
+                ViewBag.items = new SelectList(list, "IdCauHinh", "TenCauHinh");
                 return View();
+            }    
+                
             else
                 return RedirectToAction("DangNhap");
+        }
+
+        [HttpPost]
+        public ActionResult Index(FormCollection f)
+        {
+            var kq = f["ddlCauHinh"];
+            List<CauHinh> list = new List<CauHinh>();
+            foreach (var item in db.CAUHINHs.Where(x => x.Active == true))
+            {
+                CauHinh ch = new CauHinh();
+                ch.IdCauHinh = item.IdCauHinh;
+                ch.TenCauHinh = item.LOAIDETAI.TenLoai + " " + item.NIENKHOA1.NamBD;
+                list.Add(ch);
+            }
+            if (kq != "")
+            {
+                int giatri = int.Parse(kq);
+                ViewBag.items = new SelectList(list, "IdCauHinh", "TenCauHinh", giatri);
+                List<int> values = new List<int>();
+                List<string> labels = new List<string>();
+                foreach (var y in db.CHUYENNGANHs)
+                {
+                    int k = db.DETAIs.Count(m => m.ChuyenNganh == y.IdCNganh && m.CauHinh == giatri);
+                    values.Add(k);
+                    labels.Add(y.TenCNganh);
+                }
+
+                ViewBag.Label = labels;
+                ViewBag.Value = values;
+                ViewBag.GiaTri = giatri;
+            }
+            else
+            {
+                ViewBag.items = new SelectList(list, "IdCauHinh", "TenCauHinh");
+                ViewBag.Label = "";
+                ViewBag.Value = "";
+                ViewBag.GiaTri = 0;
+            }
+            return View();
         }
 
         [HttpGet]
